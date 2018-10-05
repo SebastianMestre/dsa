8bdc2dfb2fc5b728bee37cca39b2897764737873
SebastianMestre <mestresebastianunr@gmail.com>
2018-10-05 16:46:17 -0300

    separa implentacion e interfaz de bloom-filter

--- bloom-filter/bloom-filter.c
+++ bloom-filter/bloom-filter.c
@@ -1,50 +1,10 @@
 #include "bloom-filter.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct {
-    size_t size;
-    char* arr;
-} BitArray;
-
-BitArray BitArray_make ( size_t size ) {
-    int n_bytes = ( size / 8 ) + 1;
-    if( size % 8 == 0 ) n_bytes--;
-    printf( "%d %d\n", size, n_bytes );
-    BitArray ret = { size, malloc(n_bytes) };
-    memset( ret.arr, 0, n_bytes );
-    return ret;
-}
-
-
-
-void BitArray_set ( BitArray A, size_t index, bool value ) {
-    if ( value == true ) A.arr[index/8] |=  ( 1 << (index%8) );
-    else                 A.arr[index/8] &= ~( 1 << (index%8) );
-}
-
-bool BitArray_get ( BitArray A, size_t index ) {
-    return A.arr[index/8] & (1<<(index % 8));
-}
-
-// Bloom filter
-
-size_t horner ( const char* val, size_t seed, size_t size ) {
-    size_t result = 0;
-    for (size_t i = 0; val[i]; ++i)
-        result = (result * seed + val[i]) % size;
-    return result;
-}
-
-typedef struct {
-    BitArray data;
-    size_t hash_count;
-    size_t (*hash)(const char*, size_t, size_t);
-} BloomFilter;
+#include "../bit-array/bit-array.h"
+#include "../hashing/horner.h"
 
 BloomFilter BloomFilter_make (size_t size, size_t hash_count) {
     return (BloomFilter){
@@ -68,24 +28,4 @@ bool BloomFilter_query ( BloomFilter this, const char* val ) {
             return false;
     }
     return true;
-}
-
-int main ( void ) {
-
-    BloomFilter pepe = BloomFilter_make(10, 6);
-
-    BloomFilter_insert(pepe, "hola");
-    BloomFilter_insert(pepe, "antonelli");
-    BloomFilter_insert(pepe, "mestre");
-
-    printf("%d\n", BloomFilter_query(pepe, "hola"));
-    printf("%d\n", BloomFilter_query(pepe, "antonelli"));
-    printf("%d\n", BloomFilter_query(pepe, "mestre"));
-    printf("%d\n", BloomFilter_query(pepe, "nam"));
-    printf("%d\n", BloomFilter_query(pepe, "pennice"));
-    printf("%d\n", BloomFilter_query(pepe, "cori"));
-
-
-
-    return 0;
 }
--- bloom-filter/bloom-filter.h
+++ bloom-filter/bloom-filter.h
@@ -1,3 +1,17 @@
-typedef struct BitArray {} BitArray;
-typedef struct BloomFilter {} BloomFilter;
+#include <stdint.h>
+#include <stdbool.h>
 
+#include "../bit-array/bit-array.h"
+
+
+typedef struct BloomFilter{
+    BitArray data;
+    size_t hash_count;
+    size_t (*hash)(const char*, size_t, size_t);
+} BloomFilter;
+
+BloomFilter BloomFilter_make (size_t size, size_t hash_count);
+
+void BloomFilter_insert ( BloomFilter this, const char* val );
+
+bool BloomFilter_query ( BloomFilter this, const char* val );
